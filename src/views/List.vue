7c8dfbf1302a726c4869bbc7e9d1335df1abde3d
olegliallin <olegliallin@gmail.com>
2020-04-03 19:02:14 +0300

    first commit

---
+++ src/views/List.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  <h1>List</h1>
+    <hr>
+    <table v-if="tasks.length">
+      <thead>
+      <tr>
+        <th>#</th>
+        <th>Title</th>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Status</th>
+        <th>Open</th>
+      </tr>
+      </thead>
+    </table>
+    <p v-else>No tasks</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      tasks() {
+        return  this.$store.getters.tasks
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+
+</style>
